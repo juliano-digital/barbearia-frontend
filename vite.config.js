b66cb916a6987268c97fac66534cccb0f7e1ac4e
Juliano <julianoaraujo8749@gmail.com>
2026-07-22 23:53:31 -0300

    almentando memoria js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+export default defineConfig({
+  // ... suas outras configurações
+  build: {
+    chunkSizeWarningLimit: 600, // Aumenta o limite para 600 kB por exemplo
+  },
+})
