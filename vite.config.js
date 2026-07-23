7256c361e4333a84e9bd4851f17308ea6d1e903c
Juliano <julianoaraujo8749@gmail.com>
2026-07-22 23:59:08 -0300

    almentando kb js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default defineConfig({
-  // ... suas outras configurações
   build: {
-    chunkSizeWarningLimit: 600, // Aumenta o limite para 600 kB por exemplo
+    chunkSizeWarningLimit: 600, // Aumenta o limite para 600 kB
   },
 })
